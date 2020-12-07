6c5955ba85b80878caf37eb764ee55d98e0c579e
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-12-07 15:35:10 -0800

    Spaceship & Meteor added

--- Game.py
+++ Game.py
@@ -6,6 +6,34 @@ class Spaceship(pygame.sprite.Sprite):
         self.image = pygame.image.load(path)
         self.rect = self.image.get_rect(center = (x_pos, y_pos))
 
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+        self.screen_constrain()
+
+    def screen_constrain(self):
+        if self.rect.right >= 1280:
+            self.rect.right = 1280
+        if self.rect.left <=  0:
+            self.rect.left = 0
+    
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, path, x_pos, y_pos, x_speed, y_speed):
+        super().__init__()
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(center = (x_pos, y_pos))
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+        self.screen_constrain()
+
+    def screen_constrain(self):
+        if self.rect.right >= 1280:
+            self.rect.right = 1280
+        if self.rect.left <=  0:
+            self.rect.left = 0
+
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -14,6 +42,9 @@ spaceship = Spaceship("spaceship.png", 640, 500, 10)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
+meteor1 = Meteor("Meteor1.png", 450, 650, 50, 50)
+meteor_group = pygame.sprite.Group()
+meteor_group.add(meteor1)
 
 while True:
     for event in pygame.event.get():
@@ -21,7 +52,9 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill((42, 45))
+    screen.fill((42, 45, 51))
     spaceship_group.draw(screen)
+    meteor_group.draw(screen)
+    spaceship_group.update()
     pygame.display.update()
     clock.tick(120)
